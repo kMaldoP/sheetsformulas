c6bc5e8afc7f1c439f225f29ae4cbd6cea09aab9
Krys Maldo <krysm2015@gmail.com>
2021-11-15 01:44:08 -0800

    first commit

---
+++ tipsplit.py
@@ -0,0 +1,27 @@
+####create a formula that adds amount of credit card and cash tips and divides by number of bartenders on the split.
+
+
+
+class split_tips:
+    def __init__(self, cash, ccards, open_tabs, bartenders):
+        self.cash = cash
+        self.ccards = ccards
+        self.open_tabs = open_tabs
+        self.bartenders = bartenders
+
+        ### add sum of cash and credit card tips plus 10 percent of open tabs and divide by number of bartenders.
+    def first_split():
+        pass
+
+        ##### repeat formula taking in new inputs and subtracting credit card tips from previous split
+
+
+
+        
+
+
+
+cash = int(input('Enter cash total:        '))
+ccards =int(input('Enter total cc tips:     '))
+open_tabs = int(input('Enter the total of open tabs:    '))
+bartenders = int(input('Enter number of bartenders to split:    '))
