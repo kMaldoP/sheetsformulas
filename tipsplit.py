782e8a93579bb64000bde8478fcd7342bc2efa56
Krys Maldo <krysm2015@gmail.com>
2021-11-15 18:04:38 -0800

    fix positional arguments

--- tipsplit.py
+++ tipsplit.py
@@ -1,25 +1,19 @@
-####create a formula that adds amount of credit card and cash tips and divides by number of bartenders on the split.
-
-
 
 class split_tips:
-    def __init__(self, cash, ccards, open_tabs, bartenders):
+    def __init__(self, cash, ccards, open_tabs):
         self.cash = cash
         self.ccards = ccards
         self.open_tabs = open_tabs
-        self.bartenders = bartenders
+       
 
-        ### add sum of cash and credit card tips plus 10 percent of open tabs and divide by number of bartenders.
-    def first_split():
-        pass
-
-        ##### repeat formula taking in new inputs and subtracting credit card tips from previous split
-
+    def first_split(self):
+        bartenders = int(input('Enter number of bartenders to split:    '))
+        total1 = ((cash + ccards + (open_tabs * .10)) / bartenders)
+        return total1
 
 
-        
 
-
+    
 
 cash = int(input('Enter cash total:        '))
 ccards =int(input('Enter total cc tips:     '))
