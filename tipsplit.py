d9d5d0b622adc70eaeba2ec2010fdd5d684bde5a
krysmaldonado <krysm2015@gmail.com>
2022-02-05 11:43:54 -0800

    adding second split

--- tipsplit.py
+++ tipsplit.py
@@ -4,15 +4,23 @@ class SplitTips:
         self.cash = cash
         self.ccards = ccards
         self.open_tabs = open_tabs
-        self.tippercentage = self.ccards * .10
+        self.rolloverpercentage = self.open_tabs * .10
 
-    def split(self):
+    def Split1(self):
         self.bartenders = int(input('Enter number of bartenders to split:    '))
 
-        total = (self.cash + self.ccards + self.tippercentage) / self.bartenders
+        total = (self.cash + self.ccards + self.rolloverpercentage) / self.bartenders
 
         return total
 
+    def Split2(self ):
+        cash2 = self.cash - int(input("enter the amount of cash:  "))
+        ccards2 = self.ccards - float(input("enter the amount of credit cards: "))
+        opentabs2 = self.open_tabs - float(input("Enter the amount of open tabs:  "))
+        self.rolloverpercentage = opentabs2 * .10
+        self.cash = cash2
+        self.ccards = ccards2
+        self.open_tabs = opentabs2
 
 ##drive code
 day1 = SplitTips(100, 50.25, 150.25)
